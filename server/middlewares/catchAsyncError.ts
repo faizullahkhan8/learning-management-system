b0607fc42ac12946238e6c788f9440e6e50b9f35
Faiz Ullah Khan <faizullahofficial0@gmail.com>
2024-11-25 13:27:39 +0500

    inhance catch async error middleware

--- server/middlewares/catchAsyncError.ts
+++ server/middlewares/catchAsyncError.ts
@@ -1,6 +1,16 @@
 import { NextFunction, Request, Response } from "express";
 
-export const CatchAsyncError =
-    (theFunc: any) => (req: Request, res: Response, next: NextFunction) => {
+/**
+ * A utility to handle async errors in Express routes and middlewares.
+ * It ensures unhandled errors in asynchronous functions are passed to the error handler.
+ *
+ * @param {Function} theFunc - The asynchronous function to wrap.
+ * @returns {Function} A wrapped function that handles errors.
+ */
+export const CatchAsyncError = (
+    theFunc: (req: Request, res: Response, next: NextFunction) => Promise<any>
+) => {
+    return (req: Request, res: Response, next: NextFunction) => {
         Promise.resolve(theFunc(req, res, next)).catch(next);
     };
+};

d87ad49aaa5696700a7ea952168fdbe915a856b0
Faiz Ullah Khan <faizullahofficial0@gmail.com>
2024-11-29 22:26:28 +0500

    Hero completed

---
+++ client/app/components/Route/Hero.tsx
@@ -0,0 +1,81 @@
+import Image from "next/image";
+import React, { FC } from "react";
+import image from "@/public/IMG_20210901_113021.jpg";
+import { BiSearch } from "react-icons/bi";
+import Link from "next/link";
+
+type Props = {};
+
+const Hero: FC<Props> = (props) => {
+    return (
+        <div className="w-full 1000px:flex items-center">
+            {/* <div className="absolute top-[100px] 1000px:top-[unset] 1500px:h-[700px] 1500px:w-[700px] 1100px:h-[500px] 1100px:w-[500px] h-[50vh] w-[50vh] hero_animation rounded-[50%] 1100px:left-[18rem] 1500px:left-[7rem] bg-blue-600"></div> */}
+            <div className="1000px:w-[40%] flex 1000px:min-h-screen items-center justify-end max-1000px:justify-center pt-[70px] 1000px:pt-0 z-10">
+                <Image
+                    src={image}
+                    alt=""
+                    className="object-contain 1100px:max-w-[90%] w-[90%] 1500px:max-w-[85%] h-[auto] z-10 rounded-full"
+                />
+            </div>
+            <div className="1000px:w-[60%] flex flex-col items-center px-10 1000px:mt-[0px] text-center 1000px:text-left mt-[150px]">
+                <h2 className="dark:text-white text-[#000000c7] 1100px:w-[78%] text-[30px] 1000px:text-[50px] font-[600] font-Josefin py-2 1000px:leading-[75px] 1500px:w-[60%]">
+                    Improve Your Online Learning Experience Better Instantly
+                </h2>
+                <br />
+                <p className="dark:text-[#edfff4] text-[#000000ac] font-Josefin font-[550] text-[18px] 1500px:!w-[60%] 1100px:!w-[78%]">
+                    We have 40k+ Online courses & 500k+ Online registered
+                    students. Find your desired Courses form them.
+                </p>
+                <br />
+                <br />
+                <div className="1500px:w-[60%] 1100px:w-[78%] w-full h-[58px] bg-transparent relative">
+                    <input
+                        type="search"
+                        placeholder="Search Courses..."
+                        className="bg-transparent border dark:border-none dark:bg-[#575757] dark:placeholder:text-[#ffffffdd] rounded-[5px] p-2 w-full h-full outline-none text-[#0000004e] dark:text-[#ffffffe6] text-20px font-[500] font-Josefin "
+                    />
+                    <div className="absolute flex items-center justify-center w-[60px] h-full cursor-pointer right-0 top-0 bg-[#39c1f3] rounded-r-[5px]">
+                        <BiSearch className="text-white" size={30} />
+                    </div>
+                </div>
+                <br />
+                <br />
+                <div className="1500px:w-[60%] 1100px:w-[78%] w-full flex items-center">
+                    <Image
+                        src={image}
+                        width={60}
+                        height={60}
+                        alt=""
+                        className="rounded-full"
+                    />
+                    <Image
+                        src={image}
+                        width={60}
+                        height={60}
+                        alt=""
+                        className="rounded-full ml-[-20px]"
+                    />
+                    <Image
+                        src={image}
+                        width={60}
+                        height={60}
+                        alt=""
+                        className="rounded-full ml-[-20px]"
+                    />
+                    <p className="font-Josefin dark:text-[#edfff4] text-[#000000b3] 1000px:pl-3 text-[18px] font-[500]">
+                        500K+ People already trusted us.
+                        <Link
+                            href="/courses"
+                            className="dark:text-[#46e256] text-[crimson]"
+                        >
+                            View Courses
+                        </Link>
+                    </p>
+                </div>
+                <br />
+            </div>
+        </div>
+    );
+};
+
+export default Hero;
